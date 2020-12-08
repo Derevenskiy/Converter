1c7f43fc6ec2be51ce0d8202280512d5df1212be
Derevenskiy <hacker73rus@gmail.com>
2020-12-08 17:32:01 +0400

    first commit

---
+++ Currency Converter/ModelDecodable.swift
@@ -0,0 +1,9 @@
+//
+//  ModelDecodable.swift
+//  Currency Converter
+//
+//  Created by Артем Чернышов on 08.12.2020.
+//  Copyright © 2020 Artem Chernyshov. All rights reserved.
+//
+
+import Foundation
